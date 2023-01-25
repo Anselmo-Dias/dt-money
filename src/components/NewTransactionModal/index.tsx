1ef47cd4950a6f387e87192fefc6233c54dc44a9
user.name <anselmodiasyahoo@gmail.com>
2023-01-25 13:37:22 -0300

    finalização da estilização do modal e a criação do botões de entrada e saida

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,15 +1,42 @@
 import * as Dialog  from "@radix-ui/react-dialog";
-import { Close, Content, Overlay, Portal, Title } from "./styles";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
+import { Close, Content, Overlay, TransactionType, TransactionTypeButton } from "./styles";
 
 export function NewTransactionModal() {
   return (
-    <Portal>
+    <Dialog.Portal>
       <Overlay/>
-      <Content>
-        <Title>Nova Transação</Title>
+      
 
-        <Close >X</Close>
+      <Content>
+        <Close >
+            <X size={24}/>
+        </Close>
+        <Dialog.Title>Nova Transação</Dialog.Title>
+
+          <form action="">
+            <input type="text" placeholder="Descrição" required />
+            <input type="number" placeholder="Preço" required />
+            <input type="text" placeholder="Categoria" required />
+
+            <TransactionType>
+              <TransactionTypeButton variant="income" value="income">
+                <ArrowCircleUp  size={24}/>
+                Entrada
+              </TransactionTypeButton>
+           
+
+           
+              <TransactionTypeButton variant="outcome" value="outcome">
+                <ArrowCircleDown  size={24}/>
+                Saída
+              </TransactionTypeButton>
+            </TransactionType>
+
+            <button type="submit">Cadastrar</button>
+          </form>
+        
       </Content>
-    </Portal>
+    </Dialog.Portal>
   );
 }
